55aadde4911698d31759154b3abacfad01aa4d84
Steven Kelly <kelweb87@gmail.com>
2022-10-12 15:46:32 +0100

    fix indentation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Routes>
-					<Route path="/" element={<h1>Random users</h1>} />
-				</Routes>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Random users</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders header", () => {
-	render(<App />);
-	const header = screen.getByRole("heading", { name: "Random users" });
-	expect(header).toBeInTheDocument();
+  render(<App />);
+  const header = screen.getByRole("heading", { name: "Random users" });
+  expect(header).toBeInTheDocument();
 });
